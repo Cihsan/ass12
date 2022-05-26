5ec7717f0e0a559031fe9cb2aedb7f3b388c2686
Cihsan <mutamuty@gmail.com>
2022-05-27 05:32:11 +0600

    getPdId

--- index.js
+++ index.js
@@ -127,6 +127,12 @@ async function run() {
             const result = await cursor.toArray()
             res.send(result)
         })
+        app.get('/product-get/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await product.findOne(query)
+            res.send(result)
+        })
         //Delete Product from Display and database
         app.delete('/product-del/:id', async (req, res) => {
             const id = req.params.id
